20f6a7aa1ce1ee4c58aecbee0b31c6125636c2f7
SolutionsDevop <devopssolomon@gmail.com>
2024-12-26 20:18:24 -0600

    initial commit

---
+++ design.sh
@@ -0,0 +1,29 @@
+# DESIGN AND IMPLEMENT SOURCE CONTROL
+- Manage branching strategies and PR
+- Integrate repos with Azure pipelines
+- Code review 
+- Build validation
+- Configure permissions on repos
+
+# DESIGN AND IMPLEMENT BUILD AND RELEASE PIPELINES
+- Automate builds
+- Staging Environments
+- Deployments
+- Continous Deliveries
+- Artifact Management
+
+# DEVELOP SECURITY AND COMPLIANCE PLAN
+- Establish access controls
+- Enable audit loggin and monitoring
+- Ensure complaint instrasturctures settings
+- Adhere to regulations
+
+# IMPLEMENT AN INSTRUMENTATION STRATEGY
+- Incorporate telemetry
+- Monitor performance
+- Set up logging
+- Establish alerting
+- Provide dashboards and reporting
+
+# GET AZURE DEVEOPER OR AZURE ADMINISTRATION CERTIFICATES
+
---
+++ trace.sh
@@ -0,0 +1,30 @@
+# IMPLEMENTING TRACEABILITY
+- Use Azure boards
+- Link work items
+- Integrate with repos
+- Leverage pipelines
+- Utilize monitoring tools
+
+# THINGS TO KNOW FOR TRACEABILITY
+- Understance traceability principles
+    - Ensures visibility in DevOps
+    - Tracking work and feedback
+    - Measuring efficiency and recovery
+    - Linking code and work items
+    - Policy implementation and integration
+- Know the Azure tools for traceability
+- Recognize traceability in CI/CD
+- Apply real-world scenarios
+- Traceability in Agile environments
+
+# GENERATING RELEASE NOTE AND ADDING IT TO THE PIPELINE
+- step 1
+    goto https://marketplace.visualstudio.com/items?itemName=richardfennellBM.BM-VSTS-XplatGenerateReleaseNotes
+- step 2
+    install or download it we the organization does not alrady have it
+- step 3
+    generate the release note put it in a temporary area and copy it into wiki or email it out. This will be automated
+- step 4
+
+
+
