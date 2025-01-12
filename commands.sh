c1f436afc353067e1e97434e9fd454087d369fde
SolutionsDevop <devopssolomon@gmail.com>
2025-01-12 15:46:32 -0600

    added resource in bicep

---
+++ commands.sh
@@ -0,0 +1,9 @@
+# To check for typos or errors in ur bicep file
+az bicep lint --file <your-file-path>
+# To deploy a bicep template
+az deployment group create --resource-group $RESOURCE_GROUP_NAME --template-file /r
+oot/bicep-template/vnet.bicep
+# To export all resources in a resource group to an ARM template
+az group export command in Azure CLI
+
+export template option in Resource group blade
